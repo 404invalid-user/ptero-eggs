849670c5a6039b4d9aeb87d8082ecf7be1e28072
404invalid-user <invaliduser@bruvland.com>
2023-03-21 16:49:43 +0000

    ahhh

---
+++ start.sh
@@ -0,0 +1,6 @@
+rm -rf /home/container/tmp/*; 
+echo "Starting PHP-FPM";
+/usr/sbin/php-fpm81 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize;
+echo "Starting Nginx";
+echo "Successfully started"; 
+/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/;
