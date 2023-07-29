ada2d079ed5d60caacf3289cc4f35009e0d1a7ac
404invalid-user <invaliduser@bruvland.com>
2023-07-29 21:56:49 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 cd /home/container || exit 1
-
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n go build -o app"
+go build -o app
 
 # todo work out how to gownload go modules and thigns then add that code
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
